9a3d8bbac479fa06ce6ecd886461681cd92e6258
wb-zj268791 <Yuious12345>
2018-07-06 00:26:45 +0800

    logack config

--- spring-boot-samples/jetty-sample/src/main/java/org/helixcs/springboot/samples/jetty/JettyController.java
+++ spring-boot-samples/jetty-sample/src/main/java/org/helixcs/springboot/samples/jetty/JettyController.java
@@ -1,5 +1,7 @@
 package org.helixcs.springboot.samples.jetty;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,12 +10,14 @@ import javax.annotation.Resource;
 
 @Controller
 public class JettyController {
+    private static final Logger jettyLog = LoggerFactory.getLogger("jettyLog");
     @Resource
     private HelloServiceImpl helloService;
 
     @GetMapping("/jetty")
     @ResponseBody
     public String sayHello(){
+        jettyLog.info("fuck all things.");
         return helloService.sayHello();
     }
 
