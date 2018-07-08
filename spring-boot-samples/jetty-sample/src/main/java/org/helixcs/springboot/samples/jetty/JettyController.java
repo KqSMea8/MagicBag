a0803a05186edabd57a0226e5a01d177f5d646fe
Xarrow <zhangjian.dev@gmail.com>
2018-07-08 14:37:53 +0800

    fix pom

--- spring-boot-samples/jetty-sample/src/main/java/org/helixcs/springboot/samples/jetty/JettyController.java
+++ spring-boot-samples/jetty-sample/src/main/java/org/helixcs/springboot/samples/jetty/JettyController.java
@@ -2,11 +2,13 @@ package org.helixcs.springboot.samples.jetty;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class JettyController {
@@ -16,7 +18,7 @@ public class JettyController {
 
     @GetMapping("/jetty")
     @ResponseBody
-    public String sayHello(){
+    public String sayHello(HttpServletRequest request){
         jettyLog.info("fuck all things.");
         return helloService.sayHello();
     }
