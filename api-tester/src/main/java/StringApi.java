9150789250029cc25503de4321ec99f0e9ba48a7
wb-zj268791 <Yuious12345>
2018-07-10 11:35:55 +0800

    string re matcher

---
+++ api-tester/src/main/java/StringApi.java
@@ -0,0 +1,15 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.regex.Pattern;
+
+public class StringApi {
+    private  static void  stringReMatch(){
+        String string= "(MRT)84116748";
+        String substring = StringUtils.substring(string,string.indexOf(")")+1,string.length());
+        String finalString = Pattern.compile("[^0-9]").matcher(substring).replaceAll("").trim();
+        System.out.println(finalString);
+    }
+    public static void main(String[] args) {
+        stringReMatch();
+    }
+}
