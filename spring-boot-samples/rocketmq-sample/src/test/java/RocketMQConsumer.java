fd4e16862f692a080e27291a73437f2768386b26
Xarrow <zhangjian.dev@gmail.com>
2018-07-23 00:25:09 +0800

    mq

---
+++ spring-boot-samples/rocketmq-sample/src/test/java/RocketMQConsumer.java
@@ -0,0 +1,44 @@
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+/**
+ * @Author: Helixcs
+ * @Time:7/22/18
+ */
+public class RocketMQConsumer {
+    public static void main(String[] args) throws MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("consumer1");
+        consumer.setNamesrvAddr("192.168.31.174:9876");
+
+        //这里设置的是一个consumer的消费策略
+        //CONSUME_FROM_LAST_OFFSET 默认策略，从该队列最尾开始消费，即跳过历史消息
+        //CONSUME_FROM_FIRST_OFFSET 从队列最开始开始消费，即历史消息（还储存在broker的）全部消费一遍
+        //CONSUME_FROM_TIMESTAMP 从某个时间点开始消费，和setConsumeTimestamp()配合使用，默认是半个小时以前
+
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+
+        consumer.subscribe("topic1","*");
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                System.out.println(Thread.currentThread().getName() + " Receive New Messages: " + msgs);
+
+                //返回消费状态
+                //CONSUME_SUCCESS 消费成功
+                //RECONSUME_LATER 消费失败，需要稍后重新消费
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+
+        consumer.start();
+
+
+    }
+}
---
+++ spring-boot-samples/rocketmq-sample/src/test/java/RocketMQProduct.java
@@ -0,0 +1,30 @@
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * @Author: Helixcs
+ * @Time:7/22/18
+ */
+public class RocketMQProduct {
+    public static void main(String[] args) throws MQClientException, UnsupportedEncodingException, RemotingException, InterruptedException, MQBrokerException {
+        DefaultMQProducer producer = new DefaultMQProducer("p1");
+        producer.setNamesrvAddr("192.168.31.174:9876");
+        producer.start();
+
+
+        for(int i=0;i<10;i++){
+            Message message = new Message("topic1","tag1",("Hello World "+i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+
+            SendResult sendResult = producer.send(message);
+            System.out.println(sendResult);
+        }
+
+    }
+}
