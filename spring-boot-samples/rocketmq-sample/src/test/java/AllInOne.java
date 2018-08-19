0a6a0ec7167a785576db71e4ed608db551abc743
Xarrow <zhangjian.dev@gmail.com>
2018-08-20 01:51:18 +0800

    kt

--- spring-boot-samples/rocketmq-sample/src/test/java/AllInOne.java
+++
@@ -1,9 +0,0 @@
-/**
- * @Author: Helixcs
- * @Time:8/12/18
- */
-public class AllInOne {
-    public static void main(String[] args) throws Exception {
-        RocketMQBroadcasting.broadingcastingConsumer();
-    }
-}
--- spring-boot-samples/rocketmq-sample/src/test/java/MQProducer.java
+++ spring-boot-samples/rocketmq-sample/src/test/java/MQProducer.java
@@ -1,8 +1,12 @@
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.MessageQueueSelector;
 import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageQueue;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+import java.util.List;
 
 /**
  * @Author: Helixcs
@@ -11,10 +15,12 @@ import org.apache.rocketmq.remoting.common.RemotingHelper;
  */
 public class MQProducer {
 
+    private static final  String nameSrvAddr = "192.168.31.174:9876";
+
     // 短信发送
     private static  void syncProducerSendMsg() throws  Exception {
         DefaultMQProducer producer = new DefaultMQProducer("default_unique_group");
-        producer.setNamesrvAddr("192.168.31.174:9876");
+        producer.setNamesrvAddr(nameSrvAddr);
         producer.start();
 
         for(int i=0;i<100;i++){
@@ -32,7 +38,7 @@ public class MQProducer {
 
     private static void asyncProducerSendMsg() throws Exception{
         DefaultMQProducer producer = new DefaultMQProducer();
-        producer.setNamesrvAddr("192.168.31.174:9876");
+        producer.setNamesrvAddr(nameSrvAddr);
         producer.setProducerGroup("asyncGroup");
         producer.start();
         producer.setRetryTimesWhenSendAsyncFailed(0);
@@ -66,7 +72,7 @@ public class MQProducer {
     //应用:单向传输用于要求中等可靠性的情况，如日志收集
     private static void oneWaySendMsg() throws  Exception {
         DefaultMQProducer defaultMQProducer = new DefaultMQProducer("oneway_group");
-        defaultMQProducer.setNamesrvAddr("192.168.31.174:9876");
+        defaultMQProducer.setNamesrvAddr(nameSrvAddr);
         defaultMQProducer.start();
         for (int i=0;i<100;i++){
             Message message = new Message(
@@ -80,7 +86,34 @@ public class MQProducer {
         defaultMQProducer.shutdown();
     }
 
+    // order product
 
+    private static void orderProducer() throws  Exception{
+        DefaultMQProducer mqProducer = new DefaultMQProducer("orderProducerGroup");
+        mqProducer.setNamesrvAddr(nameSrvAddr);
+        mqProducer.start();
+        String [] tags = new String[]{"TagA","TagB","TagC","TagD"};
+        for (int i=0;i<100;i++){
+            int orderId = i%10;
+            System.out.println("OrderId is :"+orderId);
+            Message message = new Message("TopicOrder",
+                    tags[i%tags.length],
+                    "KEY"+i,
+                    ("Hello "+i).getBytes("utf-8"));
+            SendResult sendResult  = mqProducer.send(message, new MessageQueueSelector() {
+                @Override
+                public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
+                    Integer id = (Integer) arg;
+                    int index = id % mqs.size();
+                    return mqs.get(index);
+                }
+            },orderId);
+
+            System.out.printf("%s%n", sendResult);
+        }
+
+        mqProducer.shutdown();
+    }
 
 
     public static void main(String[] args) throws Exception {
