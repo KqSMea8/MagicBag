e8fd5930c68bbd32fc033ed712ed899fd1ccdd55
Helixcs <Yuious12345>
2018-09-04 01:47:10 +0800

    mybatis generator

--- spring-boot-samples/jetty-sample/src/main/java/org/helixcs/springboot/samples/jetty/dubboimpl/DubboSayHelloImpl.java
+++ spring-boot-samples/jetty-sample/src/main/java/org/helixcs/springboot/samples/jetty/dubboimpl/DubboSayHelloImpl.java
@@ -3,10 +3,10 @@ package org.helixcs.springboot.samples.jetty.dubboimpl;
 import org.helixcs.springboot.samples.dubboclient.SayHelloInterface;
 import org.springframework.stereotype.Service;
 
-@Service(value = "sayHelloImpl")
-public class SayHelloImpl implements SayHelloInterface {
+@Service(value = "dubboSayHelloImpl")
+public class DubboSayHelloImpl implements SayHelloInterface {
     @Override
     public String sayHello(String... args) {
-        return "Hello World";
+        return "Hello World, this message is from dubbo provider.";
     }
 }
