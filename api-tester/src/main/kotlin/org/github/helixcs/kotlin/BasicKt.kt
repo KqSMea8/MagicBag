6a0ef8b156effff1c60e6e43eab8c3c9b90d37a9
Xarrow <zhangjian12424@gmail.com>
2018-10-26 02:58:31 +0800

    Add kotlin basic samples

---
+++ api-tester/src/main/kotlin/org/github/helixcs/kotlin/BasicKt.kt
@@ -0,0 +1,47 @@
+package org.github.helixcs.kotlin
+
+/**
+ * @Author: Helixcs
+ * @Time:10/26/18
+ */
+
+// basic collection
+val numbers: MutableList<Int> = mutableListOf(1, 2, 4)
+
+val onlyReadMap = mapOf<Int, String>(1 to "one", 2 to "two", 3 to "three")
+
+val items = listOf<Int>(1, 32, 43, 43, 43)
+
+val hashSet = hashSetOf<String>("a", "das", "dsa", "b")
+
+// destructuring declarations  解构
+enum class Status {
+    SUCCESS, FAILED, EXCEPTION
+}
+
+enum class Color(val rgb: Int) {
+    RED(0xFF0000),
+    GREEN(0x00FF00)
+}
+
+data class Result(val result: Int, val status: Status)
+
+fun generateResultFunction(result: Int, status: Status): Result {
+    return Result(result, status)
+}
+
+
+fun main(args: Array<String>) {
+    numbers.add(12)
+
+    var entries = onlyReadMap.filter { it.key == 2 }.entries.firstOrNull()
+
+    println(numbers)
+    println(onlyReadMap)
+    println(entries)
+    println(items)
+    println(hashSet)
+
+    val (result, status) = generateResultFunction(1, Status.SUCCESS)
+    println("$result , $status")
+}
