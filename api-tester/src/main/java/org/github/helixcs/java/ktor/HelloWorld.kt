d698bb25f57caaf9d882c72d383fb254fa4d9667
wb-zj268791 <zhangjian12424@gmail.com>
2018-10-31 20:18:48 +0800

    Add Samples Of Ktor

--- api-tester/src/main/java/org/github/helixcs/java/ktor/HelloWorld.kt
+++ api-tester/src/main/java/org/github/helixcs/java/ktor/HelloWorld.kt
@@ -12,20 +12,22 @@ import io.ktor.application.Application
 import io.ktor.application.ApplicationCallPipeline
 import io.ktor.application.call
 import io.ktor.application.install
-import io.ktor.features.CallLogging
-import io.ktor.features.ContentNegotiation
-import io.ktor.features.DefaultHeaders
+import io.ktor.features.*
 import io.ktor.gson.gson
 import io.ktor.http.ContentType
+import io.ktor.http.withCharset
 import io.ktor.request.queryString
 import io.ktor.request.uri
 import io.ktor.request.userAgent
 import io.ktor.response.header
+import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.get
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import java.text.DateFormat
+import java.time.LocalDate
 
 /**
  * @Author: Helixcs
@@ -33,13 +35,25 @@ import io.ktor.server.netty.Netty
  * @sample :https://ktor.io/quickstart/index.html
  */
 
+data class Model(val name: String, val items: List<Item>, val date: LocalDate = LocalDate.of(2018, 10, 1))
+
+data class Item(val key: String, val value: String)
+
+val model = Model(name = "root", items = listOf(Item(key = "key1", value = "value1")))
 fun Application.mymodule() {
     install(DefaultHeaders)
     install(CallLogging)
+    install(Compression)
+    // enable HSTS
+    install(HSTS){
+        includeSubDomains= true
+    }
     // gson
-    install(ContentNegotiation){
+    install(ContentNegotiation) {
         gson {
+            setDateFormat(DateFormat.LONG)
             setPrettyPrinting()
+
         }
     }
     install(Routing) {
@@ -50,8 +64,8 @@ fun Application.mymodule() {
             val userAgent = call.request.userAgent()
             val cookies = call.request.cookies.rawCookies.entries
             var cookiesString = ""
-            for ( cookie in cookies){
-                cookiesString +="${cookie.key}=${cookie.value}&"
+            for (cookie in cookies) {
+                cookiesString += "${cookie.key}=${cookie.value}&"
             }
 
 //            call.request.
@@ -64,17 +78,23 @@ fun Application.mymodule() {
         // response samples
         get("/cos") {
             val response = call.response
-            var content:String = ""
+            var content: String = ""
             // set cookies
-            response.header("Set-Cookie","_custom_cookie=this cookie is from ktor")
+            response.header("Set-Cookie", "_custom_cookie=this cookie is from ktor")
             // custom headers
-            response.header("X-Powered","helixcs")
-            response.header("X-Powered","ktor")
+            response.header("X-Powered", "helixcs")
+            response.header("X-Powered", "ktor")
             //send application.json
-//            call.respondText("COS", ContentType.Application.Json)
+            // with se
+            call.respondText("COS 你好世界! | こんにちは世界" ,ContentType.Text.Html.withCharset(Charsets.UTF_8))
 
             // send files
 //            call.respondFile(File("C:\\Users\\wb-zj268791\\Downloads\\Go for Python Hackers - Greg Ward.mp4"))
+        }
+
+
+        get("/json") {
+            call.respond(model)
         }
     }
 
