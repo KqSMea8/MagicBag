814adfbc6021ed29261cda70ab2366ec756c01e9
wb-zj268791 <zhangjian12424@gmail.com>
2018-11-04 00:50:17 +0800

    fix

--- api-tester/src/main/kotlin/org/github/helixcs/kotlin/CoroutinesKt.kt
+++ api-tester/src/main/kotlin/org/github/helixcs/kotlin/CoroutinesKt.kt
@@ -9,18 +9,3 @@ package org.github.helixcs.kotlin
  * CPU 密集型: 多进程 -> 多线程
  *协程 coroutine 轻量级线程
  */
-
-//suspend fun worker(n:Int):Int{
-//    delay(1000L)
-//    return n
-//}
-fun main(args: Array<String>) {
-
-    // outdated
-//    launch(CommonPool) {
-//        delay(3000L, TimeUnit.MILLISECONDS)
-//        println("Hello,")
-//    }
-//    println("World!")
-//    Thread.sleep(5000L)
-}
