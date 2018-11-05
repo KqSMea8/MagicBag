dde2cae83574a46137e3a148eee1009544eee3e7
Xarrow <zhangjian12424@gmail.com>
2018-11-05 23:06:44 +0800

    add some method

--- api-tester/src/main/kotlin/org/github/helixcs/kotlin/CollectionsKt.kt
+++ api-tester/src/main/kotlin/org/github/helixcs/kotlin/CollectionsKt.kt
@@ -3,15 +3,30 @@ package org.github.helixcs.kotlin
 
 data class PersonData constructor(val name: String, val age: Int, val sex: String?)
 
-var personDataList = listOf<PersonData>(PersonData(name = "zhangsan", age = 11, sex = null),
-        PersonData(name = "lisi", age = 22, sex = "男"))
 
 // distinct method
 fun distinctMethod() {
     val simpleList = listOf<Int>(1, 1, 32, 42, 43, 3, 1, 432, 35, 3454, 45)
     println(simpleList)
     println(simpleList.distinct())
+
+
+    // distinct object
+    var personDataList = listOf<PersonData>(
+            PersonData(name = "apple", age = 11, sex = null),
+            PersonData(name = "banana", age = 22, sex = "男"),
+            PersonData(name = "cat", age = 29, sex = "female"),
+            PersonData(name = "cat", age = 29, sex = "female")
+    )
+
+    println("==> before distinct")
+    personDataList.forEach({ element -> println(element) })
+    println("==> after distinct")
+    personDataList.distinct().forEach { element -> println(element) }
+
+
 }
+
 
 fun main(args: Array<String>) {
     distinctMethod()
