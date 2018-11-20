f30946cbe41fe56fbae2a961d662b865b3773f59
wb-zj268791 <zhangjian12424@gmail.com>
2018-11-20 21:49:16 +0800

    fix pom

--- spring-boot-samples/jetty-sample/src/main/java/org/helixcs/springboot/samples/jetty/JettyApplication.java
+++ spring-boot-samples/jetty-sample/src/main/java/org/helixcs/springboot/samples/jetty/JettyApplication.java
@@ -2,10 +2,9 @@ package org.helixcs.springboot.samples.jetty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication(scanBasePackages = "org.helixcs.springboot.samples.jetty")
-@ImportResource(value = {"all_in_one.xml"})
+//@ImportResource(value = {"all_in_one.xml"})
 public class JettyApplication {
 
     public static void main(String[] args) {
