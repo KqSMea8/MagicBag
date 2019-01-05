5f52de5f6da65318bff69978c1d81528e1550a8e
Xarrow <zhangjian12424@gmail.com>
2019-01-05 21:32:53 +0800

    Add kotlin basic samples

--- api-tester/src/main/kotlin/org/github/helixcs/kotlin/CoroutinesKt.kt
+++ api-tester/src/main/kotlin/org/github/helixcs/kotlin/CoroutinesKt.kt
@@ -22,4 +22,5 @@ fun main(args: Array<String>) {
     }
     println("Hello,") // main thread continues while coroutine is delayed
     Thread.sleep(2000L) // block main thread for 2 seconds to keep JVM alive
+
 }
