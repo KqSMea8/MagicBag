a074e5e92d0fa0fdac460a4751e7c784ad757d5d
wb-zj268791 <zhangjian12424@gmail.com>
2019-01-16 23:12:40 +0800

    add StringServer

--- netty/src/main/java/org.github.helixcs.netty/StringServer.java
+++ netty/src/main/java/org.github.helixcs.netty/StringServer.java
@@ -1,13 +1,16 @@
 package org.github.helixcs.netty;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+
+import java.io.IOException;
 
 public class StringServer {
     public static void main(String[] args) {
@@ -23,7 +26,45 @@ public class StringServer {
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel ch) throws Exception {
-                            ch.pipeline().addLast(new StringServerHandler());
+                            ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                                @Override
+                                public void channelActive(ChannelHandlerContext ctx) throws Exception {
+//                                    super.channelActive(ctx);
+//                                    ctx.
+                                    System.out.println("> 服务端激活链接");
+                                }
+
+                                @Override
+                                public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                    ByteBuf byteBuf = (ByteBuf) msg;
+                                    byte[] req = new byte[byteBuf.readableBytes()];
+                                    byteBuf.readBytes(req);
+                                    String reqString = new String(req, "UTF-8");
+                                    System.out.println("> 服务端接受数据:" + reqString);
+                                    String res = getRequest(reqString);
+                                    System.out.println("> 服务端刷新数据:" + res);
+
+                                    byte[] resByte = res.getBytes();
+                                    ByteBuf wbf = Unpooled.buffer(resByte.length);
+                                    wbf.writeBytes(resByte);
+//                                    ctx.writeAndFlush(wbf);
+                                    ctx.write(wbf);
+                                }
+
+                                @Override
+                                public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+//                                    super.channelReadComplete(ctx);
+                                    ctx.writeAndFlush(Unpooled.EMPTY_BUFFER)
+                                            .addListener(ChannelFutureListener.CLOSE);
+                                }
+
+                                private String getRequest(String reqString) throws IOException {
+                                    HttpClient client = new HttpClient();
+                                    GetMethod getMethod = new GetMethod(reqString);
+                                    client.executeMethod(getMethod);
+                                    return getMethod.getResponseBodyAsString();
+                                }
+                            });
                         }
                     })
                     .option(ChannelOption.SO_BACKLOG, 1024)
--- netty/src/main/java/org.github.helixcs.netty/StringServerHandler.java
+++ netty/src/main/java/org.github.helixcs.netty/StringServerHandler.java
@@ -3,8 +3,12 @@ package org.github.helixcs.netty;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
 
-public class StringServerHandler extends ChannelInboundHandlerAdapter{
+import java.io.IOException;
+
+public class StringServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         super.channelActive(ctx);
@@ -13,14 +17,19 @@ public class StringServerHandler extends ChannelInboundHandlerAdapter{
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf byteBuf = (ByteBuf) msg;
-        byte [] req = new byte[byteBuf.readableBytes()];
+        byte[] req = new byte[byteBuf.readableBytes()];
         byteBuf.readBytes(req);
-        String reqString = new String(req,"UTF-8");
-        System.out.println("> 接受数据"+reqString);
+        String reqString = new String(req, "UTF-8");
+        System.out.println("> 接受数据" + reqString);
+        String res = getRequest(reqString);
+        ctx.writeAndFlush(res);
+        ctx.close();
+    }
 
-
-
-        
-
+    private String getRequest(String reqString) throws IOException {
+        HttpClient client = new HttpClient();
+        GetMethod getMethod = new GetMethod(reqString);
+        client.executeMethod(getMethod);
+        return getMethod.getResponseBodyAsString();
     }
 }
